a1b3a60bb51d5705de01f5a6e6285cfe2f63b3e1
Fahry Aditya <fahryadityagithub@gmail.com>
2026-05-15 08:43:14 +0800

    Update Project 20.5 last

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
     icon: 'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 1200"><rect width="1200" height="1200" rx="280" fill="%23052659"/><svg x="160" y="160" width="880" height="880" viewBox="0 -960 960 960" fill="%23ffffff"><path d="M480-120 200-272v-240L40-600l440-240 440 240v320h-80v-276l-80 44v240L480-120Zm0-332 274-148-274-148-274 148 274 148Zm0 241 200-108v-151L480-360 280-470v151l200 108Zm0-241Zm0 90Zm0 0Z"/></svg></svg>'
   },
   verification: {
-    google: 'google65a931532649ffe2'
+    google: ['google65a931532649ffe2', '9r12_Fm3WN29l9Avf2y_Vp5Uk-n-ae6uDxJ46NEdW0o']
   }
 }
 
