596fe57eba4d890efe1b039e9ada0ffc2f06bbef
Fahry Aditya <fahryadityagithub@gmail.com>
2026-05-23 11:21:01 +0800

    resolve merge conflict

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -28,7 +28,7 @@ type SidebarItem =
 function getFlattenedNavItems(role: string, isCollapsed: boolean): SidebarItem[] {
   const items: SidebarItem[] = []
   
-  items.push({ type: 'logo', label: 'Sistem Ekstrakurikuler', version: 'V 18.5.0 Artemis Series (Stable) ' })
+  items.push({ type: 'logo', label: 'Sistem Ekstrakurikuler', version: 'V 18.5.5 Artemis Series (Stable) ' })
   
   if (!isCollapsed) {
     items.push({ type: 'badge', role })
